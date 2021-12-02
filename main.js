2cf1dafa656f6baecbb99ebd0ecfca18b20239ba
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-02 22:36:55 +0100

    Adds stylesheet to lichess.org

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Running");
+let childs = document.getElementsByTagName("head")[0].childNodes;
+
+for (let i = 0; i < childs.length; i++) {
+    if (childs[i].tagName === "LINK") {
+        let href = childs[i].href;
+
+        if (href.includes("piece-css")) {
+            childs[i].href = browser.runtime.getURL("chesscom-style.css");
+        }
+    }
+}
