9077c9a4f224e5a860d1c12e24a098b2df5c754f
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-03 11:08:40 +0100

    Added black pieces

--- firefox/main.js
+++ firefox/main.js
@@ -4,5 +4,6 @@ let sheet = document.createElement('style')
 
 for (let i = 0; i < pieces.length; i++) {
     sheet.innerHTML += ".is2d ." + pieces[i][1] + ".white {background-image:url('" + browser.runtime.getURL("ressources/pieces/w" + pieces[i][0] + ".png") + "')}\n";
+    sheet.innerHTML += ".is2d ." + pieces[i][1] + ".black {background-image:url('" + browser.runtime.getURL("ressources/pieces/b" + pieces[i][0] + ".png") + "')}\n";
 }
 document.body.appendChild(sheet);
