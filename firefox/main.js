44e7a8995b46eb87e0a764b73f1100652c5a794d
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-03 11:08:17 +0100

    Changing all white pieces

--- firefox/main.js
+++ firefox/main.js
@@ -1,3 +1,8 @@
+let pieces = [["p", "pawn"], ["q", "queen"], ["r", "rook"], ["k", "king"], ["b", "bishop"], ["n", "knight"]];
+
 let sheet = document.createElement('style')
-sheet.innerHTML = ".is2d .pawn.white {background-image:url('" + browser.runtime.getURL("ressources/pieces/wp.png") + "')}";
+
+for (let i = 0; i < pieces.length; i++) {
+    sheet.innerHTML += ".is2d ." + pieces[i][1] + ".white {background-image:url('" + browser.runtime.getURL("ressources/pieces/w" + pieces[i][0] + ".png") + "')}\n";
+}
 document.body.appendChild(sheet);
