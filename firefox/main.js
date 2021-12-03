138354b14a978872816b1b6b9ac5ae5f48ddb2d5
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-03 11:17:50 +0100

    Added board replacement

--- firefox/main.js
+++ firefox/main.js
@@ -6,4 +6,5 @@ for (let i = 0; i < pieces.length; i++) {
     sheet.innerHTML += ".is2d ." + pieces[i][1] + ".white {background-image:url('" + browser.runtime.getURL("ressources/pieces/w" + pieces[i][0] + ".png") + "')}\n";
     sheet.innerHTML += ".is2d ." + pieces[i][1] + ".black {background-image:url('" + browser.runtime.getURL("ressources/pieces/b" + pieces[i][0] + ".png") + "')}\n";
 }
+sheet.innerHTML += ".is2d cg-board {background-image:url('" + browser.runtime.getURL("ressources/boards/150.png") + "') !important}\n";
 document.body.appendChild(sheet);
