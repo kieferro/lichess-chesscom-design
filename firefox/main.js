d2345f49c5a9a20ccb79b323611fc770d000ac2f
kieferro <81773954+kieferro@users.noreply.github.com>
2021-12-03 10:55:59 +0100

    Saved pieces as local files

--- firefox/main.js
+++ firefox/main.js
@@ -1,17 +1,3 @@
-let childs = document.getElementsByTagName("head")[0].childNodes;
-
-for (let i = 0; i < childs.length; i++) {
-    if (childs[i].tagName === "LINK") {
-        let href = childs[i].href;
-
-        if (href.includes("piece-css")) {
-            let new_child = childs[i].cloneNode(true);
-            new_child.href = browser.runtime.getURL("chesscom-style.css");
-
-            childs[i].remove();
-
-            document.getElementsByTagName("head")[0].appendChild(new_child);
-            break;
-        }
-    }
-}
+let sheet = document.createElement('style')
+sheet.innerHTML = ".is2d .pawn.white {background-image:url('" + browser.runtime.getURL("ressources/pieces/wp.png") + "')}";
+document.body.appendChild(sheet);
